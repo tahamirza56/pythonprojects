17aeef2e97b2fefddfa84929597697059f63bfc5
tahamirza56 <mirzataha730@outlook.com>
2024-10-19 11:54:51 +0530

    Add files via upload

---
+++ check the number of days in a given month.py
@@ -0,0 +1,37 @@
+num= int(input("enter month number:"))
+if num== 1:
+    print("january")
+    print("january has 31 days")
+elif num== 2:
+    print("february")
+    print("february has 28 and 29 days based on leap year")
+elif num== 3:
+    print("march")
+    print("march has 31 days")
+elif num== 4:
+    print("april")
+    print("april has 30 days")
+elif num== 5:
+    print("may")
+    print("may has 31 days")
+elif num== 6:
+    print("june")
+    print("june has 30 days")
+elif num== 7:
+    print("july")
+    print("july has 31 days")
+elif num== 8:
+    print("august")
+    print("august has 31 days")
+elif num== 9:
+    print("september")
+    print("september has 30 days")
+elif num== 10:
+    print("october")
+    print("october has 31 days")
+elif num== 11:
+    print("november")
+    print("november has 30 days")
+elif num== 12:
+    print("december")
+    print("december has 31 days")
